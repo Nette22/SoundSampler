834fc0b39b697c4f408920b51b779cdafedfeb5f
Alban <a.dericbourg+github@gmail.com>
2011-11-23 13:53:44 +0100

    Initial commit.

---
+++ SoundSampler/src/com/dericbourg/apps/mobile/soundsampler/SoundSamplerActivity.java
@@ -0,0 +1,13 @@
+package com.dericbourg.apps.mobile.soundsampler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SoundSamplerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
