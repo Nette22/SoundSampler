a3f836456cdbd972e3fd3cddd80947c2cc1eb18b
Alban Dericbourg <alban.dericbourg+git@gadz.org>
2011-12-24 18:30:46 +0100

    Bought dericbourg.name domain andupdated Eclipse conf.
    Signed-off-by: Alban Dericbourg <alban.dericbourg+git@gadz.org>

---
+++ SoundSampler/src/name/dericbourg/apps/mobile/soundsampler/SoundSamplerActivity.java
@@ -0,0 +1,15 @@
+package name.dericbourg.apps.mobile.soundsampler;
+
+import name.dericbourg.apps.mobile.soundsampler.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SoundSamplerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
