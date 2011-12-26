25443f4c341fae6a34cb276263d70e00c8e800c3
Alban <a.dericbourg+github@gmail.com>
2011-12-26 14:24:07 +0100

    Let's have a try on ORMLite. Might be wrong way...

---
+++ SoundSampler/src/name/dericbourg/apps/mobile/soundsampler/db/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package name.dericbourg.apps.mobile.soundsampler.db;
+
+import name.dericbourg.apps.mobile.soundsampler.core.exception.NotImplementedException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Helper for database.
+ * 
+ * @author adericbourg
+ */
+final class DatabaseHelper {
+
+	private DatabaseHelper() {
+		// Utility class.
+	}
+
+	/**
+	 * On create.
+	 * 
+	 * @param database Database.
+	 * @param connectionSource Datasource.
+	 */
+	static void onCreate(final SQLiteDatabase database, final ConnectionSource connectionSource) {
+		throw new NotImplementedException();
+	}
+
+	/**
+	 * On upgrade.
+	 * 
+	 * @param database Database.
+	 * @param connectionSource Datasource.
+	 * @param oldVersion Old version.
+	 * @param newVersion New version.
+	 */
+	static void onUpgrade(final SQLiteDatabase database, final ConnectionSource connectionSource, final int oldVersion,
+			final int newVersion) {
+		throw new NotImplementedException();
+	}
+}
